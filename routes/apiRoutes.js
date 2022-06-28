28b5dde7af63009975127d6d0f0e2fbf5a0e60e0
puppetup <savinohoshizaki@gmail.com>
2022-06-27 21:13:06 -0700

    FORGOT TO MAKE REPO BEFORE I STARTED AHHHHH

---
+++ routes/apiRoutes.js
@@ -0,0 +1,58 @@
+const api = require('express').Router();
+const { application } = require('express');
+const db = require('../db/db.json');
+const fs = require('fs');
+
+api.get('/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+        console.log(JSON.parse(data))
+        }
+    }
+    )
+});
+
+// POST Route for submitting feedback
+api.post('/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const {title,text} = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+      };
+  
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(newNote);
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedData, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully added note')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+
+module.exports = api
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
